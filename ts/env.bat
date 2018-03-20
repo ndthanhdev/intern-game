e4db50c49925f779b1a2d3b1ee3f5c41b9bb62ce
Duy Thanh Nguyen <thanhtdt@outlook.com>
2018-03-20 14:46:19 +0700

    pharse 2
    intergrate provide

--- ts/env.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-SET VCAP_APPLICATION={"start":"2018-03-03 20:14:32 +0700","application_id":"c23faef6-b416-44af-9344-23eccc2c25c4","instance_id":"c23faef6-b416-44af-9344-23eccc2c25c4","space_id":"8ebccc8b-beaf-40df-acdb-774bddd5f77c","application_name":"ts","organization_name":"dev","space_name":"DEV","started_at_timestamp":"1520082872789","started_at":"2018-03-03 20:14:32 +0700","state_timestamp":"1520081309335","full_application_uris":["http://sgnl50846993a.dhcp.sgn.sap.corp:51033"],"application_uris":["sgnl50846993a.dhcp.sgn.sap.corp:51033"],"uris":["sgnl50846993a.dhcp.sgn.sap.corp:51033"],"version":"667883ba-16a8-46bf-b9e1-7447e2992ef9","application_version":"667883ba-16a8-46bf-b9e1-7447e2992ef9"}
-SET VCAP_SERVICES={}
-SET MEMORY_LIMIT=1024m
